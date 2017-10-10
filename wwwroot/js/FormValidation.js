e0f9b21e3e9eec5be10f9c2a1c1eb4dce8d6be0a
Joseph Tomlinson and Rane Fields <joseph+rane@>
2017-10-10 16:41:32 -0700

    Make page not refresh if form info is invalid

---
+++ wwwroot/js/FormValidation.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("form#add-task").submit(function(event) {
+    var taskName = $("#task-name").val();
+    var taskDescription = $("#task-description").val();
+    if (taskName === "") {
+      event.preventDefault();
+    }
+    if (taskDescription === "") {
+      event.preventDefault();
+    }
+  });
+});
