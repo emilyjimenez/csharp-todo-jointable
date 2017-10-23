f5c52f759310dd01f6bed916b68e876877dbea9a
Guest User <Guest@Epicodus-1E.local>
2017-10-23 14:28:23 -0700

    Add save and find methods

--- ToDoList.Tests/Models.Tests/TaskTests.cs
+++ ToDoList.Tests/Models.Tests/TaskTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
 using System;
+using System.Collections.Generic;
 
 namespace ToDoList.Models.Tests
 {
@@ -13,7 +14,7 @@ namespace ToDoList.Models.Tests
         }
         public void Dispose()
         {
-            //Task.DeleteAll();
+            Task.ClearAll();
         }
 
         [TestMethod]
@@ -22,6 +23,64 @@ namespace ToDoList.Models.Tests
             int result = Task.GetAll().Count;
 
             Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void Equals_ReturnsTrueIfDescriptionsAreTheSame_Task()
+        {
+            Task firstTask = new Task("Mow the lawn", "Make sure you mow the lawn");
+            Task secondTask = new Task("Mow the lawn", "Make sure you mow the lawn");
+
+            Assert.AreEqual(firstTask, secondTask);
+        }
+        [TestMethod]
+        public void Save_SavesToDatabase_TaskList()
+        {
+            // Arrange
+            Task testTask = new Task("Mow the lawn", "3 cm exactly");
+
+            // Act
+            testTask.Save();
+            List<Task> result = Task.GetAll();
+            List<Task> testList = new List<Task>{testTask};
+
+            // Assert
+            CollectionAssert.AreEqual(testList, result);
+        }
+        [TestMethod]
+        public void Save_AssignsIdToObject_Id()
+        {
+            Task testTask = new Task("Mow the lawn", "3 cm exactly");
+            testTask.Save();
+
+            Task savedTask = Task.GetAll()[0];
+
+            int result = savedTask.Id;
+            int testId = testTask.Id;
+
+            Assert.AreEqual(testId, result);
+        }
+        [TestMethod]
+        public void Save_AssignsIdToMultipleObjects_Ids()
+        {
+            Task task1 = new Task("Mow the lawn", "3 cm exactly");
+            task1.Save();
+            Task task2 = new Task("Water the plants", "but not too much");
+            task2.Save();
+
+            List<Task> allTasks = Task.GetAll();
+
+            Assert.AreEqual(allTasks[0].Id, task1.Id);
+            Assert.AreEqual(allTasks[1].Id, task2.Id);
+        }
+        [TestMethod]
+        public void Find_FindsTaskInDatabase_Task()
+        {
+            Task testTask = new Task("Mow the lawn", "3 cm exactly");
+            testTask.Save();
+
+            Task foundTask = Task.Find(testTask.Id);
+
+            Assert.AreEqual(testTask, foundTask);
         }
     }
 }
--- ToDoList/Models/Task.cs
+++ ToDoList/Models/Task.cs
@@ -6,8 +6,8 @@ namespace ToDoList.Models
 {
     public class Task
     {
-        public string Name {get; set;}
-        public string Description {get; set;}
+        public string Name {get; private set;}
+        public string Description {get; private set;}
         public int Id {get; private set;}
 
         public Task (string name, string description, int id = 0)
@@ -41,11 +41,101 @@ namespace ToDoList.Models
         }
         public static void ClearAll()
         {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
 
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM tasks";
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+        public void Save()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"INSERT INTO `tasks` (`name`, `description`) VALUES (@TaskName, @TaskDescription);";
+
+            MySqlParameter name = new MySqlParameter();
+            name.ParameterName = "@TaskName";
+            name.Value = this.Name;
+            cmd.Parameters.Add(name);
+
+            MySqlParameter description = new MySqlParameter();
+            description.ParameterName = "@TaskDescription";
+            description.Value = this.Description;
+            cmd.Parameters.Add(description);
+
+            cmd.ExecuteNonQuery();
+            Id = (int)cmd.LastInsertedId;
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
         }
         public static Task Find(int searchId)
         {
-            return new Task("name", "description", 0);
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText =  @"SELECT * FROM `tasks` WHERE id = @thisId;";
+
+            MySqlParameter thisId = new MySqlParameter();
+            thisId.ParameterName = "@thisId";
+            thisId.Value = searchId;
+
+            cmd.Parameters.Add(thisId);
+
+            int taskId = 0;
+            string taskName = "";
+            string taskDescription = "";
+
+            var rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while(rdr.Read())
+            {
+                taskId = rdr.GetInt32(0);
+                taskName = rdr.GetString(1);
+                taskDescription = rdr.GetString(2);
+            }
+
+            Task foundTask = new Task(taskName, taskDescription, taskId);
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return foundTask;
+        }
+
+        public override bool Equals(System.Object otherTask)
+        {
+            if (!(otherTask is Task))
+            {
+                return false;
+            }
+            else
+            {
+                Task newTask = (Task) otherTask;
+                bool idEquality = (this.Id == newTask.Id);
+                bool nameEquality = (this.Name == newTask.Name);
+                bool descriptionEquality = (this.Description == newTask.Description);
+                return (idEquality && nameEquality && descriptionEquality);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
         }
     }
 }
